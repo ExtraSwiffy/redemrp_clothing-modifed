81dee0d261033686bbb0091ca0754bf2e31d8cfb
ExtraSwiffy <68062448+ExtraSwiffy@users.noreply.github.com>
2022-02-01 22:30:07 -0500

    Add files via upload

---
+++ redemrp_clothing-modified/config.lua
@@ -0,0 +1,54 @@
+Config = {}
+Config.BlipName = 'Clothing Store' -- Blip Name Showed on map
+Config.EnableCommand = true -- Enable/Disable /clothing command
+Config.BlipSprite = 1195729388	 -- Clothing shop sprite
+Config.BlipScale = 0.2 -- Blip scale
+Config.OpenKey = 0xD9D0E1C0 -- Opening key hash
+Config.Price = 5 -- Price for clothes
+Config.Zones = {
+	vector3(-325.78, 806.22, 117.88) -- VALENTINE
+
+}
+
+
+Config.Label = {
+		["boot_accessories"] = "Boot Accessories",
+ 		["pants"] = "Pants",            	
+ 		["cloaks"] = "Cloak",            	
+ 		["hats"] = "Hat",             	
+ 		["vests"] = "Vest",            	
+ 		["chaps"] = "Chaps",             	
+ 		["shirts_full"] = "Shirt",       	
+ 		["badges"] = "Badge",           	
+ 		["masks"] = "Mask",            	
+ 		["spats"] = "Spats",           	
+ 		["neckwear"] = "Neck",         	
+ 		["boots"] = "Boots",            	
+ 		["accessories"] = "Accessories",      	
+ 		["jewelry_rings_right"] = "Jewelry Right",
+ 		["jewelry_rings_left"]	= "Jewelry Left",
+ 		["jewelry_bracelets"] = "Bracelets",	
+ 		["gauntlets"] = "Gauntlets",        	
+ 		["neckties"] = "Necktie",        	
+ 		["holsters_knife"] = "Knife Holster",   	
+ 		["talisman_holster"] = "Talisman Holster",
+ 		["loadouts"] = "Loadouts",          	
+ 		["suspenders"] = "Suspenders",      	
+ 		["talisman_satchel"] = "Talisman Satchel", 	
+ 		["satchels"] = "Satchel",         	
+ 		["gunbelts"] = "Gunbelt",          	
+ 		["belts"] = "Belt",            	
+ 		["belt_buckles"] = "Belt Buckle",     	
+ 		["holsters_left"] = "Holster Left",    	
+ 		["holsters_right"] = "Holster Right",   	
+ 		["talisman_wrist"] = "Wrist Talisman",   	
+ 		["coats"] = "Coat",            	
+ 		["coats_closed"] = "Coat Closed",     	
+ 		["ponchos"] = "Poncho",           	
+ 		["eyewear"] = "Eyewear",          	
+ 		["gloves"] = "Gloves",           	
+ 		["holsters_crossdraw"] = "Crossdraw Holster", 	
+ 		["aprons"] = "Aprons",			
+ 		["skirts"] = "Skirts",          	
+
+}
